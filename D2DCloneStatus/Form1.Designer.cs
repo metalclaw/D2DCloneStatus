debbff47d4f207fca34bb3b3de3d4f153aee7221
Stevan Panić <zeratul11@hotmail.com>
2023-02-20 19:41:12 +0100

    initial commit

---
+++ D2DCloneStatus/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace D2DCloneStatus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.olvData = new BrightIdeasSoftware.ObjectListView();
+            this.olvcRegion = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvcProgress = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvcLadder = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvcHC = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvcLastReport = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            ((System.ComponentModel.ISupportInitialize)(this.olvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // olvData
+            // 
+            this.olvData.AllColumns.Add(this.olvcRegion);
+            this.olvData.AllColumns.Add(this.olvcProgress);
+            this.olvData.AllColumns.Add(this.olvcLadder);
+            this.olvData.AllColumns.Add(this.olvcHC);
+            this.olvData.AllColumns.Add(this.olvcLastReport);
+            this.olvData.AllowColumnReorder = true;
+            this.olvData.CellEditUseWholeCell = false;
+            this.olvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvcRegion,
+            this.olvcProgress,
+            this.olvcLadder,
+            this.olvcHC,
+            this.olvcLastReport});
+            this.olvData.Cursor = System.Windows.Forms.Cursors.Default;
+            this.olvData.FullRowSelect = true;
+            this.olvData.HideSelection = false;
+            this.olvData.Location = new System.Drawing.Point(12, 12);
+            this.olvData.Name = "olvData";
+            this.olvData.Size = new System.Drawing.Size(496, 426);
+            this.olvData.TabIndex = 1;
+            this.olvData.UseCompatibleStateImageBehavior = false;
+            this.olvData.View = System.Windows.Forms.View.Details;
+            // 
+            // olvcRegion
+            // 
+            this.olvcRegion.AspectName = "region";
+            this.olvcRegion.Text = "Region";
+            this.olvcRegion.Width = 71;
+            // 
+            // olvcProgress
+            // 
+            this.olvcProgress.AspectName = "progress";
+            this.olvcProgress.Text = "Progress";
+            // 
+            // olvcLadder
+            // 
+            this.olvcLadder.AspectName = "ladder";
+            this.olvcLadder.Text = "Ladder/Non-Ladder";
+            this.olvcLadder.Width = 110;
+            // 
+            // olvcHC
+            // 
+            this.olvcHC.AspectName = "hc";
+            this.olvcHC.Text = "Hardcore/Softcore";
+            this.olvcHC.Width = 108;
+            // 
+            // olvcLastReport
+            // 
+            this.olvcLastReport.AspectName = "lastReport";
+            this.olvcLastReport.Text = "Last Report";
+            this.olvcLastReport.Width = 141;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.olvData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.olvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView olvData;
+        private BrightIdeasSoftware.OLVColumn olvcRegion;
+        private BrightIdeasSoftware.OLVColumn olvcProgress;
+        private BrightIdeasSoftware.OLVColumn olvcLadder;
+        private BrightIdeasSoftware.OLVColumn olvcHC;
+        private BrightIdeasSoftware.OLVColumn olvcLastReport;
+    }
+}
+
---
+++ D2DCloneStatus/Form1.cs
@@ -0,0 +1,159 @@
+﻿using BrightIdeasSoftware;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace D2DCloneStatus
+{
+    public partial class Form1 : Form
+    {
+        static string endpoint = "https://diablo2.io/dclone_api.php";
+        static HttpClient client = new HttpClient();
+        private List<Response> responseList = new List<Response>();
+        public Form1()
+        {
+            InitializeComponent();
+            refreshStatus();
+        }
+
+        public async void refreshStatus()
+        {
+            responseList = await getData();
+            updateList(responseList);
+        }
+
+        private async Task<List<Response>> getData()
+        {
+            List<Response> list = new List<Response>();
+
+            HttpResponseMessage response = await client.GetAsync(endpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+
+                if (json != null)
+                {
+                    list = JsonConvert.DeserializeObject<List<Response>>(json);
+                }
+            }
+
+            return list;
+        }
+
+        private void updateList(List<Response>results)
+        {
+            olvData.Items.Clear();
+
+            olvData.SetObjects(convertResultsForOLV(results));
+        }
+
+        private List<ResponseForOLV> convertResultsForOLV(List<Response> results)
+        {
+            List<ResponseForOLV> list = new List<ResponseForOLV>();
+
+            foreach (Response result in results)
+            {
+                list.Add(result.convertForOLV());
+            }
+
+            return list;
+        }
+
+        public static string parseTimestamp(double timestamp)
+        {
+            TimeSpan difference = DateTime.Now - Helpers.UnixTimeStampToDateTime(timestamp);
+
+            if (difference.Days> 0)
+            {
+                return difference.Days.ToString() + " days ago";
+            }
+            if (difference.Hours> 0)
+            {
+                return difference.Hours.ToString() + " hours ago";
+            }
+            if (difference.Minutes> 0)
+            {
+                return difference.Minutes.ToString() + " minutes ago";
+            }
+            
+            return difference.Seconds.ToString() + " seconds ago";
+        }
+
+       
+    }
+
+    public static class Helpers
+    {
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+
+    public class Response
+    {
+        public int progress { get; set; }
+        public int region { get; set; }
+        public int ladder { get; set; }
+        public int hc { get; set; }
+        public int timestamped { get; set; }
+        public int reporter_id { get; set; }
+
+        public ResponseForOLV convertForOLV()
+        {
+            ResponseForOLV response = new ResponseForOLV();
+
+            response.region = ((RegionMapping)region).ToString();
+            response.progress = (progress).ToString() + "/6";
+            response.ladder = ((LadderMapping)ladder).ToString();
+            response.hc = ((HCMapping)hc).ToString();
+            response.lastReport = Form1.parseTimestamp(timestamped);
+
+            return response;
+        }
+    }
+
+    public class ResponseForOLV
+    {
+        public string region { get; set; }
+        public string progress { get; set; }
+        public string ladder { get; set; }
+        public string hc { get; set; }
+        public string lastReport { get; set; }
+    }
+
+    enum RegionMapping
+    {
+        None,
+        Americas,
+        Europe,
+        Asia
+    }
+
+    enum LadderMapping
+    { 
+        None, 
+        Ladder, 
+        NonLadder
+    }
+
+    enum HCMapping
+    {
+        None,
+        Hardcore,
+        Softcore
+    }
+}
